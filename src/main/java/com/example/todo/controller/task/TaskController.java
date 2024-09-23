f0b49216519b0d85799df76aa5172382369b2e2a
pocari1210 <pocari.design@gmail.com>
2024-09-23 22:06:52 +0900

    LOMBOK(コンストラクタ生成)

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -1,21 +1,25 @@
 package com.example.todo.controller.task;
 
 import com.example.todo.service.task.TaskService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 // コントローラーとして認識させる
 @Controller
+
+@RequiredArgsConstructor
+
 public class TaskController {
 
     //private final TaskService taskService = new TaskService();
 
     private final TaskService taskService;
 
-    public TaskController(TaskService taskService) {
-        this.taskService = taskService;
-    }
+//    public TaskController(TaskService taskService) {
+//        this.taskService = taskService;
+//    }
 
     // データを取得する
     // メソッドとGETの処理を行うURLを紐づける役割を担う
