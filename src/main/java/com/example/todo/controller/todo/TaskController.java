62e6f1167735d94ea746ba9791c69d28099df5e2
pocari1210 <pocari.design@gmail.com>
2024-09-23 13:55:12 +0900

    Controllerからtemplateにデータを渡す

--- src/main/java/com/example/todo/controller/todo/TaskController.java
+++ src/main/java/com/example/todo/controller/todo/TaskController.java
@@ -1,6 +1,7 @@
 package com.example.todo.controller.todo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 // コントローラーとして認識させる
@@ -10,7 +11,8 @@ public class TaskController {
     // データを取得する
     // メソッドとGETの処理を行うURLを紐づける役割を担う
     @GetMapping("/tasks")
-    public String list() {
+    public String list(Model model) {
+        model.addAttribute("task", "Spring Bootを学ぶ");
         return "tasks/list";
     }
 }
