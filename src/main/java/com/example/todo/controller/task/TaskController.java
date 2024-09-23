8e692bd1ee7c121b1696b312c3af6be6bf03bbe4
pocari1210 <pocari.design@gmail.com>
2024-09-23 16:06:51 +0900

    servise層を作成してビジネスロジック分離

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -1,35 +1,22 @@
 package com.example.todo.controller.task;
 
+import com.example.todo.service.task.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 // コントローラーとして認識させる
 @Controller
 public class TaskController {
 
+    private final TaskService taskService = new TaskService();
+
     // データを取得する
     // メソッドとGETの処理を行うURLを紐づける役割を担う
     @GetMapping("/tasks")
     public String list(Model model) {
-        var task1 = new TaskDTO(
-                1L,
-                "Spring Boot を学ぶ",
-                "TODO アプリケーションを作ってみる",
-                "TODO"
-        );
-        var task2 = new TaskDTO(
-                2L,
-                "Spring Security を学ぶ",
-                "ログイン機能を作ってみる",
-                "TODO"
-        );
 
-        var taskList = List.of(task1, task2);
-
-        model.addAttribute("taskList", taskList);
+        model.addAttribute("taskList", taskService.find());
         return "tasks/list";
     }
 }
