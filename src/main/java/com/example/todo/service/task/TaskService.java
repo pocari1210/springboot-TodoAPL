9455a346ed96f6c05e2cd8f78c7b5ddaa89aec7a
pocari1210 <pocari.design@gmail.com>
2024-09-24 17:02:50 +0900

    ServiceとRepositoryの修正

--- src/main/java/com/example/todo/service/task/TaskService.java
+++ src/main/java/com/example/todo/service/task/TaskService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +16,9 @@ public class TaskService {
 
     public List<TaskEntity> find() {
         return taskRepository.select();
+    }
+
+    public Optional<TaskEntity> findById(long taskId) {
+        return taskRepository.selectById(taskId);
     }
 }
