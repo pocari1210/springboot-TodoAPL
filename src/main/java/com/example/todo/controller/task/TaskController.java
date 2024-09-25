fb32466125da8f6f355881a348ffffb5566f885f
pocari1210 <pocari.design@gmail.com>
2024-09-25 16:31:31 +0900

    課題作成画面疎通

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -49,4 +49,9 @@ public class TaskController {
         model.addAttribute("task", TaskDTO.toDTO(taskEntity));
         return "tasks/detail";
     }
+
+    @GetMapping("/tasks/creationForm")
+    public String showCreationForm() {
+        return "tasks/form";
+    }
 }
