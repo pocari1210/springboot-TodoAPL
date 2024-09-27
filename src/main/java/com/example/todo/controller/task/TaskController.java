ba0770db3636a42b207f2a79008cba631c12b558
pocari1210 <pocari.design@gmail.com>
2024-09-27 17:23:42 +0900

    存在しないIDを404ページで表示

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -47,9 +47,12 @@ public class TaskController {
 
     @GetMapping("/{id}")
     public String showDetail(@PathVariable("id") long taskId, Model model) {
+
         var taskEntity = taskService.findById(taskId)
-                .orElseThrow(() -> new IllegalArgumentException("Task not found: id = " + taskId));
+                .orElseThrow(TaskNotFoundException::new);
+
         model.addAttribute("task", TaskDTO.toDTO(taskEntity));
+
         return "tasks/detail";
     }
 
@@ -74,7 +77,7 @@ public class TaskController {
     public String showEditForm(@PathVariable("id") long id, Model model) {
 
         var taskEntity = taskService.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Task not found"));
+                .orElseThrow(TaskNotFoundException::new);
 
         var form = new TaskForm(taskEntity.summary(), taskEntity.description(), taskEntity.status().name());
 
