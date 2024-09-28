7715b811a6f2ea6d3c03fb99fb504b94937e0abf
pocari1210 <pocari.design@gmail.com>
2024-09-28 20:49:37 +0900

    リファクタリング(EntityをFormに変換)

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -1,17 +1,12 @@
 package com.example.todo.controller.task;
 
-import com.example.todo.service.task.TaskSearchEntity;
 import com.example.todo.service.task.TaskService;
-import com.example.todo.service.task.TaskStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
 
 // コントローラーとして認識させる
 @Controller
@@ -35,12 +30,8 @@ public class TaskController {
     @GetMapping
     public String list(TaskSearchForm searchForm,Model model) {
 
-        var statusEntityList = Optional.ofNullable(searchForm.status())
-                .map(statusList -> statusList.stream().map(TaskStatus::valueOf).toList())
-                .orElse(List.of());
+        var taskList = taskService.find(searchForm.toEntity())
 
-        var searchEntity = new TaskSearchEntity(searchForm.summary(), statusEntityList);
-        var taskList = taskService.find(searchEntity)
                 // リストの中身を変換
                 .stream()
 
