75c5c12c3c285749223ac1fce90d6e01b356d721
pocari1210 <pocari.design@gmail.com>
2024-09-28 17:35:40 +0900

    検索条件をServiceに渡す

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -1,13 +1,17 @@
 package com.example.todo.controller.task;
 
+import com.example.todo.service.task.TaskSearchEntity;
 import com.example.todo.service.task.TaskService;
+import com.example.todo.service.task.TaskStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.DeleteMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 // コントローラーとして認識させる
 @Controller
@@ -31,8 +35,12 @@ public class TaskController {
     @GetMapping
     public String list(TaskSearchForm searchForm,Model model) {
 
-        var taskList = taskService.find()
+        var statusEntityList = Optional.ofNullable(searchForm.status())
+                .map(statusList -> statusList.stream().map(TaskStatus::valueOf).toList())
+                .orElse(List.of());
 
+        var searchEntity = new TaskSearchEntity(searchForm.summary(), statusEntityList);
+        var taskList = taskService.find(searchEntity)
                 // リストの中身を変換
                 .stream()
 
