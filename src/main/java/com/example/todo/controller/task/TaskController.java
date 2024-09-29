c0552e949caf2762e875480be3716532e2697a75
pocari1210 <pocari.design@gmail.com>
2024-09-29 11:03:25 +0900

    概要検索の入力値を検索後も保持

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -42,6 +42,7 @@ public class TaskController {
                 .toList();
 
         model.addAttribute("taskList", taskList);
+        model.addAttribute("searchDTO", searchForm.toDTO());
         return "tasks/list";
     }
 
